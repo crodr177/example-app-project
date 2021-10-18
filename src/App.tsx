4f5c26aebbd171799192b0afdc0db089a2decf54
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2021-10-18 01:55:15 -0700

    add HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -16,14 +16,14 @@ import {
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       <title>{APP_TITLE}</title>
       <Header/>
       <Switch>
         <Route exact path="/" render={(props) => <Home title={PAGE_TITLE_HOME} {...props} />} />
-        <Route exact path="/about" render={(props) => <About title={PAGE_TITLE_ABOUT} {...props} />} />
-        <Route exact path="/contact" render={(props) => <Contact title={PAGE_TITLE_CONTACT} {...props} />} />
-        <Route exact path="/info" render={(props) => <Info title={PAGE_TITLE_INFO} {...props} />} />
+        <Route path="/about" render={(props) => <About title={PAGE_TITLE_ABOUT} {...props} />} />
+        <Route path="/contact" render={(props) => <Contact title={PAGE_TITLE_CONTACT} {...props} />} />
+        <Route path="/info" render={(props) => <Info title={PAGE_TITLE_INFO} {...props} />} />
       </Switch>
     </Router>
   );
